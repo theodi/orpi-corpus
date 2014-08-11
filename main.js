3d4e073e9db71711093335748f1e1886cea2db65
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-08-11 08:41:33 +0100

    Added function getLatLon

--- main.js
+++ main.js
@@ -10,14 +10,41 @@ var es = require('event-stream'),
 			}));
 	});
 
-request.get('http://datafeeds.networkrail.co.uk/ntrod/SupportingFileAuthenticate?type=CORPUS', {
-		'followAllRedirects': true,
-		'auth': {
-		    'user': process.env.NROD_USERNAME,
-		    'pass': process.env.NROD_PASSWORD,
-		    'sendImmediately': true
-		}
-	}, function (error, response, body) {
-		request.get(response.request.uri.href).pipe(gunzip).pipe(data);
-});
+// Uses OSM's Nominatim service to get the latitude and longitude of the best
+// match for searchString
+var getLatLon = function (searchString, callback) {
+	request.get({
+			'url': 'http://nominatim.openstreetmap.org/search', 
+			'qs': { 'format': 'json', 'q': searchString },
+			'json': true
+		},
+		function (error, response, body) {
+			if (error) {
+				callback(error);
+				return;
+			}
+			if (body.length === 0) {
+				// the call to the API could not find anything
+				callback(new Error("Nominatim did not return any results."));
+				return;
+			}
+			callback(error, { 'lat': body[0].lat, 'lon': body[0].lon });
+		});
+};
 
+var run = function () {
+	request.get('http://datafeeds.networkrail.co.uk/ntrod/SupportingFileAuthenticate?type=CORPUS', {
+			'followAllRedirects': true,
+			'auth': {
+			    'user': process.env.NROD_USERNAME,
+			    'pass': process.env.NROD_PASSWORD,
+			    'sendImmediately': true
+			}
+		}, function (error, response, body) {
+			request.get(response.request.uri.href).pipe(gunzip).pipe(data);
+	 	});	
+}
+
+getLatLon("berkhamsted railway station", function (err, latLon) {
+	console.log(latLon);
+});
